a65e63a6219d5d6ddf8debe8ac8d6a5719c0353f
FilovetsAlexandr <sohan.boxan@gmail.com>
2024-08-06 11:52:58 +0300

    initial commit

---
+++ VictoryChallenges/VictoryChallenges/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VictoryChallenges
+//
+//  Created by Alexandr Filovets on 6.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VictoryChallenges/VictoryChallenges/VictoryChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VictoryChallengesApp.swift
+//  VictoryChallenges
+//
+//  Created by Alexandr Filovets on 6.08.24.
+//
+
+import SwiftUI
+
+@main
+struct VictoryChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
